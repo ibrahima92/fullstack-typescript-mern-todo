65d4d238e883fe4a28bb3590c90b042f6f2e5f6c
kwegorek <ka.wegorek@gmail.com>
2021-03-20 19:48:35 +0100

    Refactor

--- client/server/src/controllers/todos/index.ts
+++ client/server/src/controllers/todos/index.ts
@@ -1,5 +1,5 @@
 import { Response, Request } from "express";
-import { ITodo } from "./../../types/todo";
+import { ITodo } from "../../types/todo";
 import Todo from "../../models/todo";
 
 const getTodos = async (req: Request, res: Response): Promise<void> => {
